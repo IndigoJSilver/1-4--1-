70c173ddfa60e16c46b7e99ded3720cadebac8b1
IndigoJSilver <giyeun5597@gmail.com>
2020-12-22 17:33:27 +0900

    Initial Commit

---
+++ 1-4-(1)/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1-4-(1)
+//
+//  Created by namjieun on 2020/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
